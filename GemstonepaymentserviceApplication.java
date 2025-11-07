dd01c2e93294a47c88c3f51fd6194da724a775cd
ravirajedesai <ravirajedesai@gmail.com>
2025-11-07 16:55:15 +0530

    Add files via upload

---
+++ GemstonepaymentserviceApplication.java
@@ -0,0 +1,13 @@
+package com.orderproducepayment.gemstonepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemstonepaymentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemstonepaymentserviceApplication.class, args);
+	}
+
+}
